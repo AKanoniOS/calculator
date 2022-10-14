9e47d76bb33aaf12f3205f9bf6d5bf9e4f5e504d
Steve Schmeltzer <schmeltzersteve@gmailcom>
2022-10-14 12:22:02 -0800

    add initial zero and if statement to remove leading zero from firstNum

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 let display = document.querySelector('.display')
+let firstNum = 0
+let secondNum = 0
+let firstNumDisplayTest = document.querySelector('.firstNum')
+let secondNumDisplayTest = document.querySelector('.secondNum')
+let selectedOperatorDisplayTest = document.querySelector('.selectedOperator')
+
+let add = document.querySelector('.add')
+let subtract = document.querySelector('.subtract')
+let multiply = document.querySelector('.multiply')
+let divide = document.querySelector('.divide')
+
+display.textContent = firstNum
 
 function updateDisplay(input){
+    // remove leading zeros
     display.textContent += input
+    if (Array.from(display.textContent)[0] == ('0' || 0)) {
+        display.textContent = display.textContent.slice(1)
+    }
 }
 
 function clearDisplay(input){
-    display.textContent = ``
+    display.textContent = 0
+    firstNum = 0
+    secondNum = 0
+    selectedOperatorDisplayTest = ''
+    firstNumDisplayTest.textContent = firstNum
+    secondNumDisplayTest.textContent = secondNum
+    selectedOperatorDisplayTest = selectedOperatorDisplayTest
 }
 
 let numbers = document.querySelectorAll('.num')
@@ -23,3 +45,29 @@ clear.addEventListener('click', () => {
     clearDisplay('')
 })
 
+function loadFirstNum() {
+    firstNum = display.textContent
+    firstNumDisplayTest.textContent = `${firstNum}`
+}
+
+function addFunc(){
+    loadFirstNum()
+    selectedOperatorDisplayTest.textContent = `add`
+}
+function subtractFunc(){
+    loadFirstNum()
+    selectedOperatorDisplayTest.textContent = `subtract`
+}
+function multiplyFunc(){
+    loadFirstNum()
+    selectedOperatorDisplayTest.textContent = `multiply`
+}
+function divideFunc(){
+    loadFirstNum()
+    selectedOperatorDisplayTest.textContent = `divide`
+}
+
+add.addEventListener('click', addFunc)
+subtract.addEventListener('click', subtractFunc)
+multiply.addEventListener('click', multiplyFunc)
+divide.addEventListener('click', divideFunc)
